1653722f36408b6df7cac72174aa2e698aadf961
cristian <crisdarroca@hotmail.com>
2021-08-02 19:33:14 -0500

    Hooks basico creados

---
+++ src/hooks/useNearScreen.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState, useRef } from "react"
+
+export const useNearScreen = () => {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    Promise.resolve(
+      typeof window.IntersectionObserver !== "undefined"
+        ? window.IntersectionObserver
+        : import("intersection-observer")
+    ).then(() => {
+      const observer = new window.IntersectionObserver(
+        (entries) => {
+          const { isIntersecting } = entries[0]
+          if (isIntersecting) {
+            setShow(true)
+            observer.disconnect()
+          }
+        },
+        { rootMargin: "50px" }
+      )
+      //indicando que observar
+      observer.observe(element.current)
+    })
+  }, [element])
+
+  return [show, element]
+}
