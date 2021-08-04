b211be494adfd74aa5b84a53c58a8ac0f65b26a2
cristian <crisdarroca@hotmail.com>
2021-08-03 22:19:18 -0500

    carga de query individual

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,24 @@ import { ListOfCategories } from "./components/ListOfCategories"
 import { GlobalStyles } from "./style/GlobalStyles"
 import { ListOfPhotoCards } from "./components/ListOfPhotoCards"
 import { Logo } from "./components/Logo"
-
+import { PhotoCardWithQuery } from "./container/PhotoCardWithQuery"
 const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get("detail")
+  console.log(detailId)
+
   return (
     <>
       <GlobalStyles />
       <Logo />
-      <ListOfCategories />
-      <ListOfPhotoCards categoryId={2} />
+      {detailId ? (
+        <PhotoCardWithQuery id={detailId} />
+      ) : (
+        <>
+          <ListOfCategories />
+          <ListOfPhotoCards categoryId={1} />
+        </>
+      )}
     </>
   )
 }
