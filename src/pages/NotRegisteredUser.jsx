29f6af930db826ef54a676dd72b2c410618fa9f6
cristian <crisdarroca@hotmail.com>
2021-08-08 15:29:32 -0500

    change in the NotRegisterUser

--- src/pages/NotRegisteredUser.jsx
+++ src/pages/NotRegisteredUser.jsx
@@ -1,50 +1,51 @@
-import React from "react"
-import Context from "../Context"
+import React, { useContext } from "react"
+import { Context } from "../Context"
 import { UserForm } from "../components/UserForm"
 import { useRegisterMutation } from "../hooks/useRegisterMutation"
 import { useLoginMutation } from "../hooks/useLoginMutation"
 
 export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context)
   const { mutation, mutationLoading, mutationError } = useRegisterMutation()
   const { loginMutation, loginMutationLoading, loginMutationError } =
     useLoginMutation()
+
+  const onSubmit = ({ email, password }) => {
+    const input = { email, password }
+    const variables = { input }
+    mutation({ variables }).then(({ data }) => {
+      const { signup } = data
+      activateAuth(signup)
+    })
+  }
+
+  const loginSubmit = ({ email, password }) => {
+    loginMutation({ variables: { input: { email, password } } }).then(
+      ({ data }) => {
+        const { login } = data
+        activateAuth(login)
+      }
+    )
+  }
+
+  const errorMsgReg = mutationError && "El usaurio ya existe o hay algun error"
+
+  const errorMsgLog =
+    loginMutationError && "El usaurio o contraseña incorrecto "
   return (
-    <Context.Consumer>
-      {({ activateAuth }) => {
-        const onSubmit = ({ email, password }) => {
-          const input = { email, password }
-          const variables = { input }
-          mutation({ variables }).then(activateAuth)
-        }
-
-        const loginSubmit = ({ email, password }) => {
-          loginMutation({ variables: { input: { email, password } } }).then(
-            activateAuth
-          )
-        }
-
-        const errorMsgReg =
-          mutationError && "El usaurio ya existe o hay algun error"
-
-        const errorMsgLog =
-          loginMutationError && "El usaurio o contraseña incorrecto "
-        return (
-          <>
-            <UserForm
-              disabled={mutationLoading}
-              error={errorMsgReg}
-              onSubmit={onSubmit}
-              title="Registrarte"
-            />
-            <UserForm
-              disabled={loginMutationLoading}
-              error={errorMsgLog}
-              onSubmit={loginSubmit}
-              title="Iniciar Sesión"
-            />
-          </>
-        )
-      }}
-    </Context.Consumer>
+    <>
+      <UserForm
+        disabled={mutationLoading}
+        error={errorMsgReg}
+        onSubmit={onSubmit}
+        title="Registrarte"
+      />
+      <UserForm
+        disabled={loginMutationLoading}
+        error={errorMsgLog}
+        onSubmit={loginSubmit}
+        title="Iniciar Sesión"
+      />
+    </>
   )
 }
