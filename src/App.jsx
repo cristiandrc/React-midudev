48b57f7f63e990901219e29ce380cc52b93098f1
cristian <crisdarroca@hotmail.com>
2021-08-08 22:28:51 -0500

    add Redirect en las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,37 @@
-import React from "react"
+import React, { useContext } from "react"
+import { Redirect, Router } from "@reach/router"
 import { GlobalStyles } from "./style/GlobalStyles"
 import { Logo } from "./components/Logo"
 import { Home } from "./pages/Home"
-import { Router } from "@reach/router"
 import { Detail } from "./pages/Detail"
 import { NavBar } from "./components/NavBar"
 import { Fav } from "./pages/Fav"
 import { User } from "./pages/User"
 import { NotRegisteredUser } from "./pages/NotRegisteredUser"
+import { NotFound } from "./pages/NotFound"
 
-import Context from "./Context"
+import { Context } from "./Context"
 
 const App = () => {
   // const urlParams = new window.URLSearchParams(window.location.search) // filtro del Search "/?detail=0"
   // const detailId = urlParams.get("detail")
-
+  const { isAuth } = useContext(Context)
   return (
     <>
       <GlobalStyles />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path="/" />
         <Home path="/pet/:id" />
         <Detail path="/detail/:detailId" />
+        {!isAuth && <NotRegisteredUser path="/login" />}
+        {!isAuth && <Redirect noThrow from="/fav" to="/login" />}
+        {!isAuth && <Redirect noThrow from="/user" to="/login" />}
+        {isAuth && <Redirect noThrow from="/login" to="/" />}
+        <Fav path="/fav" />
+        <User path="/user" />
       </Router>
-      <Context.Consumer>
-        {({ isAuth }) =>
-          isAuth ? (
-            <Router>
-              <Fav path="/fav" />
-              <User path="/user" />
-            </Router>
-          ) : (
-            <Router>
-              <NotRegisteredUser path="/fav" />
-              <NotRegisteredUser path="/user" />
-            </Router>
-          )
-        }
-      </Context.Consumer>
       <NavBar />
     </>
   )
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const NotFound = () => {
+  return <h1>Esta Pagina No existe :(</h1>
+}
