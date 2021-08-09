b8528dbbb0ea90e2129279a0d5a6a76e48ccc7c6
cristian <crisdarroca@hotmail.com>
2021-08-08 22:11:23 -0500

    add logout in User

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,5 +1,27 @@
-import React from "react"
+import React, { useContext } from "react"
+import styled from "styled-components"
+import { Context } from "../Context"
+
+const Button = styled.button`
+  background-color: #8d00ff;
+  border-radius: 3px;
+  color: #fff;
+  height: 32px;
+  display: block;
+  margin: 0 auto;
+  width: 90%;
+  text-align: center;
+  &[disabled] {
+    opacity: 0.3;
+  }
+`
 
 export const User = () => {
-  return <h1>User</h1>
+  const { removeAuth } = useContext(Context)
+  return (
+    <>
+      <h1>User</h1>
+      <Button onClick={removeAuth}>Cerrar Sesión</Button>
+    </>
+  )
 }
