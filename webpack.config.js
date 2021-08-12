0c89d7f8cd34b70d1a7894e143702a7cd90270d0
cristian <crisdarroca@hotmail.com>
2021-08-11 22:11:32 -0500

    Configuracion de las PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const WebpackPwaManifestPlugin = require("webpack-pwa-manifest")
+const path = require("path")
 
 module.exports = {
   output: {
@@ -8,6 +10,21 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+
+    new WebpackPwaManifestPlugin({
+      name: "Petgran - Tu app de fotos de mascotas",
+      shortname: "Petgram",
+      description: "Con Petgram puedes encontar fotos de animales domesticos",
+      background_color: "#fff",
+      theme_color: "#b1a",
+      icons: [
+        {
+          src: path.resolve("src/assets/icon.png"),
+          size: [96, 128, 192, 256, 384, 512],
+          purpose: "any maskable",
+        },
+      ],
     }),
   ],
   resolve: {
